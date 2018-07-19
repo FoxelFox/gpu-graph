739276d0b753018b6d8d35ad2d53565da83c8dc2
André Furchner <storkner@gmail.com>
2018-07-19 21:05:32 +0200

    setup for basic shader

--- src/context.ts
+++ src/context.ts
@@ -1,6 +1,10 @@
+import {shader} from "./shader/shader";
+
 export const canvas = document.getElementById("c") as HTMLCanvasElement;
 export const gl = canvas.getContext("webgl2") as WebGLRenderingContext;
 
 if (!gl) {
     console.log("No WebGL 2 for you!");
 }
+
+shader.create(require("./shader/basic/basic.vs.glsl"), require("./shader/basic/basic.fs.glsl"));
---
+++ src/shader/shader.ts
@@ -0,0 +1,48 @@
+import {gl} from "../context";
+
+class Shader {
+    create(vertexSource: string, fragmentSource: string) {
+        const vs = this.compile(gl.VERTEX_SHADER, vertexSource);
+        const fs = this.compile(gl.FRAGMENT_SHADER, fragmentSource);
+
+        if (vs && fs) {
+            return this.link(vs, fs);
+        } else {
+            // cleanup
+            if(vs) {
+                gl.deleteShader(vs);
+            }
+            if(fs) {
+                gl.deleteShader(fs);
+            }
+        }
+    }
+
+    private link(vs: WebGLShader, fs: WebGLShader): WebGLProgram {
+        const program = gl.createProgram();
+
+        gl.attachShader(program, vs);
+        gl.attachShader(program, fs);
+        gl.linkProgram(program);
+
+        if(gl.getProgramParameter(program, gl.LINK_STATUS)) {
+            return program
+        }
+
+        console.error(gl.getProgramInfoLog(program));
+        gl.deleteProgram(program);
+    }
+
+    private compile(type: number, source: string): WebGLShader {
+        const shader = gl.createShader(type);
+        gl.shaderSource(shader, source);
+        gl.compileShader(shader);
+        if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+            return shader;
+        }
+        console.error(gl.getShaderInfoLog(shader), source);
+        gl.deleteShader(shader);
+    }
+}
+
+export const shader = new Shader();
