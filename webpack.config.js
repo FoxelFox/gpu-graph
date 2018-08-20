1f5e92224b9c36bef69e1611c3997883fd972490
Robert Obermeier <obbi@u5b.de>
2018-08-20 16:27:12 +0000

    apply smaller changes to build chain

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,35 @@
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry:  "./src/main.ts",
-    output: {
-        filename: 'dist/bundle.js',
-        path: __dirname
-    },
-    devtool: 'inline-source-map',
-    resolve: {
-        extensions: ['.html', '.ts']
-    },
-    module: {
-        rules: [
-            { enforce: 'pre', test: /\.ts$/, use: "source-map-loader"},
-
-            { test: /\.ts$/, loader: 'ts-loader' },
-            { test: /\.glsl$/, loader: 'shader-loader' },
-        ]
-    },
-    plugins: [
-        //new UglifyJSPlugin(),
-        new CopyWebpackPlugin([
-            {from: 'src/main.html', to: 'dist/index.html'}
-        ])
+  entry: {
+    main: "./src/main.ts"
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: __dirname + "/public"
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".html", ".ts"]
+  },
+  module: {
+    rules: [
+      { enforce: "pre", test: /\.js$/, use: "source-map-loader" },
+      { test: /\.ts$/, loader: "awesome-typescript-loader" },
+      { test: /\.glsl$/, loader: "shader-loader" },
+      { test: /\.css$/, loader: [MiniCssExtractPlugin.loader, "css-loader"] }
     ]
+  },
+  mode: "production",
+  plugins: [
+    new MiniCssExtractPlugin({
+      chunkFilename: "[id].css",
+      filename: "[name].css"
+    }),
+    new HTMLWebpackPlugin({
+      template: "src/main.html",
+      xhtml: true
+    })
+  ]
 };
