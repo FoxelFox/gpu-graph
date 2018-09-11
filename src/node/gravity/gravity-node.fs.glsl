c6358eb5a1e701651dfc2b4cae364206172b4614
Andre Furchner <storkner@gmail.com>
2018-09-11 19:41:32 +0200

    N-body

--- src/node/gravity/gravity-node.fs.glsl
+++ src/node/gravity/gravity-node.fs.glsl
@@ -15,18 +15,33 @@ void main() {
     vec2 velocity = vec2(o.z, o.w);
 
     float radius = distance(mouse, position);
-    float force = clamp(1.0 / (radius * radius), -800.0, 800.0);
+    float force = clamp(100.0 / (radius * radius), -800.0, 800.0);
 
-    velocity -= normalize(position - mouse) * force * 0.000001 * forceActive;
+    velocity -= normalize(position - mouse) * force * 0.000000001 * forceActive;
+
+    for (int x = 0; x < 256; x++) {
+        for (int y = 0; y < 256; y++) {
+
+            vec2 p = texelFetch(image, ivec2(x, y), 0).xy;
 
 
+            if (!(p.x == position.x && p.y == position.y)) {
+                float radius = distance(p, position);
+
+
+                float force = clamp(100.0 / (radius * radius), -800.0, 800.0);
+
+                velocity -= normalize(position - p) * force * 0.0000000000001;
+
+
+            }
+        }
+    }
 
 
     position += velocity;
 
-    if (length(position) > 0.8 ) {
-        velocity *= (1.0 - length(position) * 0.01);
-    }
+
 
 
     outColor = vec4(position, velocity);
